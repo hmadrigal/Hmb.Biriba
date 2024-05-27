606baa9b821a2f15e5010856d9b7c39dd9c3090a
Herber Madrigal <hfmad@hotmail.com>
2024-05-26 22:31:14 -0600

    Subject: Integrated Dependency Injection and updated data models
    Body:
    Integrated Microsoft.Extensions.DependencyInjection package into Hmb.Biriba.ConsoleApp.csproj and added a project reference to Hmb.Biriba.csproj. Updated Program.cs to use this package for creating a service collection and building a service provider. The updated program reads a HAR file from a URL and writes each parametric request to the console, replacing the previous "Hello, World!" message.
    
    In Models.cs, commented out unnecessary properties of various record types and modified some properties to reflect changes in the data structure. For instance, the `version` property of `Log` now defaults to "1.1", and the `time` property of `Entry` has been changed from `int` to `double?`.

--- Hmb.Biriba.ConsoleApp/Program.cs
+++ Hmb.Biriba.ConsoleApp/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Hmb.Biriba.DocumentFormats.HttpArchive;
+using Hmb.Biriba.FileSystem;
+using Hmb.Biriba.Models;
+using Hmb.Biriba.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+
+
+ServiceCollection services = new ServiceCollection();
+services.AddHttpClient();
+services.AddSingleton<FileReader>();
+services.AddSingleton<HttpArchiveReader>();
+services.AddSingleton<JsonSerialization>(); 
+IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+//string url = @"https://raw.githubusercontent.com/okta/okta-sdk-test-server/master/scenarios/user-change-password.har";
+string url = @"https://raw.githubusercontent.com/tetreum/clicktrade-etf/main/insomnia_request.har";
+FileReader fileReader = serviceProvider.GetRequiredService<FileReader>();
+using Stream fileStream = await fileReader.GetStreamAsync(url);
+HttpArchiveReader httpArchiveReader = serviceProvider.GetRequiredService<HttpArchiveReader>();
+
+await foreach (ParametricRequest parametricRequest in httpArchiveReader.GetParametricRequestsAsync(fileStream))
+{
+    Console.WriteLine(parametricRequest);
+}
+
+Console.WriteLine("Press ESC to exit");
+while (Console.ReadKey().Key != ConsoleKey.Escape) ;
--- Hmb.Biriba/DoumentFormats/HttpArchive/Models.cs
+++ Hmb.Biriba/DoumentFormats/HttpArchive/Models.cs
@@ -10,43 +10,43 @@
 
     public record Log
     {
-        public string version { get; init; }
-        public Creator creator { get; init; }
-        public Page[] pages { get; init; }
+        public string version { get; init; } = "1.1";
+        //public Creator creator { get; init; }
+        //public Page[] pages { get; init; }
         public Entry[] entries { get; init; }
     }
 
-    public record Creator
-    {
-        public string name { get; init; }
-        public string version { get; init; }
-    }
+    //public record Creator
+    //{
+    //    public string name { get; init; }
+    //    public string version { get; init; }
+    //}
 
-    public record Page
-    {
-        public string startedDateTime { get; init; }
-        public string id { get; init; }
-        public string title { get; init; }
-        public PageTimings pageTimings { get; init; }
-    }
+    //public record Page
+    //{
+    //    public string startedDateTime { get; init; }
+    //    public string id { get; init; }
+    //    public string title { get; init; }
+    //    public PageTimings pageTimings { get; init; }
+    //}
 
-    public record PageTimings
-    {
-        public int onContentLoad { get; init; }
-        public int onLoad { get; init; }
-    }
+    //public record PageTimings
+    //{
+    //    public int onContentLoad { get; init; }
+    //    public int onLoad { get; init; }
+    //}
 
     public record Entry
     {
-        public string startedDateTime { get; init; }
-        public int time { get; init; }
+        //public string startedDateTime { get; init; }
+        //public double? time { get; init; }
         public Request request { get; init; }
-        public Response response { get; init; }
-        public Cache cache { get; init; }
-        public Timings timings { get; init; }
-        public string serverIPAddress { get; init; }
-        public string connection { get; init; }
-        public string pageref { get; init; }
+        //public Response response { get; init; }
+        //public Cache cache { get; init; }
+        //public Timings timings { get; init; }
+        //public string serverIPAddress { get; init; }
+        //public string connection { get; init; }
+        //public string pageref { get; init; }
     }
 
     public record Request
@@ -58,8 +58,8 @@
         public QueryString[] queryString { get; init; }
         public PostData postData { get; init; }
         public Cookie[] cookies { get; init; }
-        public Header[] headersSize { get; init; }
-        public int bodySize { get; init; }
+        //public long headersSize { get; init; } = -1;
+        //public long bodySize { get; init; }
     }
 
     public record Header
@@ -80,18 +80,18 @@
     {
         public string mimeType { get; init; }
         public string text { get; init; }
-        public Param[] @params { get; init; }
-        public string comment { get; init; }
+        //public Param[] @params { get; init; }
+        //public string comment { get; init; }
     }
 
-    public record Param
-    {
-        public string name { get; init; }
-        public string value { get; init; }
-        public string fileName { get; init; }
-        public string contentType { get; init; }
-        public string comment { get; init; }
-    }
+    //public record Param
+    //{
+    //    public string name { get; init; }
+    //    public string value { get; init; }
+    //    public string fileName { get; init; }
+    //    public string contentType { get; init; }
+    //    public string comment { get; init; }
+    //}
 
     public record Cookie
     {
@@ -104,44 +104,44 @@
         public bool secure { get; init; }
     }
 
-    public record Response
-    {
-        public int status { get; init; }
-        public string statusText { get; init; }
-        public string httpVersion { get; init; }
-        public Header[] headers { get; init; }
-        public Cookie[] cookies { get; init; }
-        public Content content { get; init; }
-        public string redirectURL { get; init; }
-        public int headersSize { get; init; }
-        public int bodySize { get; init; }
-        public string comment { get; init; }
-    }
+    //public record Response
+    //{
+    //    public int status { get; init; }
+    //    public string statusText { get; init; }
+    //    public string httpVersion { get; init; }
+    //    public Header[] headers { get; init; }
+    //    public Cookie[] cookies { get; init; }
+    //    public Content content { get; init; }
+    //    public string redirectURL { get; init; }
+    //    public int headersSize { get; init; }
+    //    public int bodySize { get; init; }
+    //    public string comment { get; init; }
+    //}
 
-    public record Content
-    {
-        public int size { get; init; }
-        public int compression { get; init; }
-        public string mimeType { get; init; }
-        public string text { get; init; }
-    }
+    //public record Content
+    //{
+    //    public int size { get; init; }
+    //    public int compression { get; init; }
+    //    public string mimeType { get; init; }
+    //    public string text { get; init; }
+    //}
 
-    public record Cache
-    {
-        public string beforeRequest { get; init; }
-        public string afterRequest { get; init; }
-    }
+    //public record Cache
+    //{
+    //    public string beforeRequest { get; init; }
+    //    public string afterRequest { get; init; }
+    //}
 
-    public record Timings
-    {
-        public int blocked { get; init; }
-        public int dns { get; init; }
-        public int connect { get; init; }
-        public int send { get; init; }
-        public int wait { get; init; }
-        public int receive { get; init; }
-        public int ssl { get; init; }
-    }
+    //public record Timings
+    //{
+    //    public int blocked { get; init; }
+    //    public int dns { get; init; }
+    //    public int connect { get; init; }
+    //    public int send { get; init; }
+    //    public int wait { get; init; }
+    //    public int receive { get; init; }
+    //    public int ssl { get; init; }
+    //}
 
 #nullable enable
 
